c39d3f67108fa7bd4287b7e78cdb79eb9a2d8390
Jenny <jennybell42@gmail.com>
2021-06-08 16:53:21 +0100

    ready to merge

--- interface.js
+++ interface.js
@@ -1,15 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
 
 var addToNotes = document.querySelector('#add-note');
-var noteItem = document.querySelector('#note');
+var note = document.querySelector('#note');
 var notelist = document.querySelector('#notelist')
 
 addToNotes.addEventListener('submit', event => {
   event.preventDefault();
-  if (noteItem.value.length < 1) return ;
-  notelist.innerHTML += ('<li>' + noteItem.value + '</li>');
-  noteItem.value = '';
+  if (note.value.length < 1) return ;
+  notelist.innerHTML += ('<li>' + note.value + '</li>');
+  note.value = '';
   localStorage.setItem('notelistItems', notelist.innerHTML);
 }, false);
+
+var saved = localStorage.getItem('notelistItems');
+
+if (saved) {
+	notelist.innerHTML = saved;
+}
 
 });
