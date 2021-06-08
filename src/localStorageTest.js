199aa8150b1801c0fdfea8f312aa53c6b411049e
Jenny <jennybell42@gmail.com>
2021-06-08 14:47:47 +0100

    test

--- src/localStorageTest.js
+++ src/localStorageTest.js
@@ -1,3 +1,5 @@
+
+
 document.addEventListener("DOMContentLoaded", () => {
 
 var addToWatchlist = document.querySelector('#add-to-watchlist');
@@ -6,21 +8,17 @@ var watchlist = document.querySelector('#watchlist')
 
 addToWatchlist.addEventListener('submit', event => {
   event.preventDefault();
-
   if (watchlistItem.value.length < 1) return ;
-
   watchlist.innerHTML += ('<li>' + watchlistItem.value + '</li>');
-
   watchlistItem.value = '';
-
   localStorage.setItem('watchlistItems', watchlist.innerHTML);
-
 }, false);
 
-});
 
 var saved = localStorage.getItem('watchlistItems');
 
 if (saved) {
   watchlist.innerHTML = saved;
 }
+
+});
