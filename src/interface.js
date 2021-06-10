e059771aebbca9d45f6c7f18c65d0ae9cd6359a0
Maria Valero <mvalerogonzalez@gmail.com>
2021-06-10 17:11:32 +0100

    WIP for popup with note

--- src/interface.js
+++ src/interface.js
@@ -4,17 +4,42 @@ var addToNotes = document.querySelector('#add-note');
 var note = document.querySelector('#note');
 var notelist = document.querySelector('#notelist');
 var notebook = new Notebook();
+var noteChoice = document.querySelector('#noteChoice')
 
 addToNotes.addEventListener('submit', (event) => {
   event.preventDefault();
   notebook.addNote(note.value);
   notelist.innerHTML += ('<li><a href="#' + notebook.showNotesLength() + '">' + notebook.showNotePreview() + '...</a></li>');
+
+   //notelist.innerHTML += ('<li><a href="#' + notebook.showNotesLength() + '">' + notebook.showNotePreview() +
+   //'...' + onclick="window.open(" + notebook.showNotes(); +", 'popup');' + '</a></li>');
+  // noteChoice.innerHTML = notebook.showNotes();
+  // console.log(notebook);
   note.value = '';
 })
 
-notelist.addEventListener('click', () => {
- console.log(url)
+addToNotes.addEventListener('onclick', (event) => {
+  event.preventDefault();
+  notebook.addNote(note.value);
+  notelist.innerHTML = ('<p>' + notebook.showNotes() + '</p>');
+  note.value = '';
 })
+
+// $(window).load(function () {
+//   $(".trigger_popup_fricc").click(function(){
+//      $('.hover_bkgr_fricc').show();
+//   });
+//   $('.hover_bkgr_fricc').click(function(){
+//       $('.hover_bkgr_fricc').hide();
+//   });
+//   $('.popupCloseButton').click(function(){
+//       $('.hover_bkgr_fricc').hide();
+//   });
+// });
+
+// notelist.addEventListener('click', () => {
+//  console.log(url)
+// })
 
 // addToNotes.addEventListener('submit', event => {
 //   event.preventDefault();
