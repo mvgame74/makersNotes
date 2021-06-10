5fe72f5837e432de4ef70dbc9df36aecd4e4fe10
charlie-casserly <charliecasserly215@gmail.com>
2021-06-10 17:40:44 +0100

    ID's can be returned to the console

--- src/interface.js
+++ src/interface.js
@@ -4,17 +4,22 @@ var addToNotes = document.querySelector('#add-note');
 var note = document.querySelector('#note');
 var notelist = document.querySelector('#notelist');
 var notebook = new Notebook();
+let idd
+const getId = async () => {}
 
 addToNotes.addEventListener('submit', (event) => {
   event.preventDefault();
   notebook.addNote(note.value);
-  notelist.innerHTML += ('<li><a href="#' + notebook.showNotesLength() + '">' + notebook.showNotePreview() + '...</a></li>');
+  notelist.innerHTML += (`<li><a href=""><div id="${notebook.showNotesLength()}">${notebook.showNotePreview()}...</div></a></li>`);
   note.value = '';
 })
 
-notelist.addEventListener('click', () => {
- console.log(url)
+notelist.addEventListener('click', (event) => {
+  event.preventDefault();
+  console.log(event.target.id)
 })
+
+
 
 // addToNotes.addEventListener('submit', event => {
 //   event.preventDefault();
