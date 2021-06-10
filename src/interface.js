0d369fa519dc52fc7efad14697c26cde449bf26c
charlie-casserly <charliecasserly215@gmail.com>
2021-06-10 12:55:45 +0100

    made minor progress

--- src/interface.js
+++ src/interface.js
@@ -2,20 +2,30 @@ document.addEventListener("DOMContentLoaded", () => {
 
 var addToNotes = document.querySelector('#add-note');
 var note = document.querySelector('#note');
-var notelist = document.querySelector('#notelist')
+var notelist = document.querySelector('#notelist');
+var notebook = new Notebook();
 
-addToNotes.addEventListener('submit', event => {
+addToNotes.addEventListener('submit', (event) => {
   event.preventDefault();
-  if (note.value.length < 1) return ;
-  notelist.innerHTML += ('<li>' + note.value + '</li>');
+  notebook.addNote(note.value);
+  notelist.innerHTML += ('<li><a href="#' + (notebook.notes).length + '">' + (notebook.notes[(notebook.notes).length - 1]).note + '</a></li>');
   note.value = '';
-  localStorage.setItem('notelistItems', notelist.innerHTML);
-}, false);
+})
 
-var saved = localStorage.getItem('notelistItems');
 
-if (saved) {
-	notelist.innerHTML = saved;
-}
+
+// addToNotes.addEventListener('submit', event => {
+//   event.preventDefault();
+//   if (note.value.length < 1) return ;
+//   notelist.innerHTML += ('<li>' + note.value + '</li>');
+//   note.value = '';
+//   localStorage.setItem('notelistItems', notelist.innerHTML);
+// }, false);
+
+// var saved = localStorage.getItem('notelistItems');
+
+// if (saved) {
+//   notelist.innerHTML = saved;
+// }
 
 });
--- src/notebook.js
+++ src/notebook.js
@@ -6,11 +6,18 @@ class Notebook {
   }
 
   addNote(note) {
+    if (note.length < 1) { throw new Error('Type something!') }
     (this.notes).push(new Note(note))
 
-    localStorage.note = JSON.stringify({text: "this is an example"});
+    // localStorage.note = JSON.stringify({text: "this is an example"});
 
-    let note = JSON.parse( localStorage.note );
-    alert( note.text ); 
+    // let note = JSON.parse( localStorage.note );
+    // alert( note.text ); 
+  }
+
+  showNotes(notelist) {
+    for (let i = 0; i < (this.notes).length; i++) {
+      
+    }
   }
 } 
